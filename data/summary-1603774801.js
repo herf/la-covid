3daea58d8c9e37a003f818fe865f6ce19692f924
Michael Herf <mbherf@gmail.com>
2020-10-26 22:00:14 -0700

    Daily update

---
+++ data/summary-1603774801.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+    var data = {
+				    "content": {            
+				               "count": "300,614", 
+								"death": "7,000" ,
+								"dailycount": "861",
+								"dailydeath": "8" ,
+								"date":"10/26" ,
+								"info":"through 8:00pm 10/25/2020" ,
+								
+								"testingaverage":"14,676" , 
+								"peopletested":"3,028,866" , 
+								"positivity":"3.4%" , 
+								"hospitalizations":"767"	
+								
+				        }       
+				};	
+				
+				
+				var content = data.content;
+				
+				$('#ctn').html(content.count);
+				$('#det').html(content.death);
+				$('#dctn').html(content.dailycount);
+				$('#ddet').html(content.dailydeath);
+				$('#dte').html(content.date);
+				$('#dte_1').html(content.date);
+				$('#dte_2').html(content.date);
+				$('#dte_3').html(content.date);
+				$('#cse').html(content.info);	
+				
+				$('#tstavg').html(content.testingaverage);	
+				$('#ppltst').html(content.peopletested);	
+				$('#pos').html(content.positivity);	
+				$('#hosp').html(content.hospitalizations);
+			
+							
+
+
+});
+
+
+
