47f5a65e91f71bde8fae49bee74e1d9266b2bf67
Michael Herf <mbherf@gmail.com>
2020-11-09 20:10:12 -0800

    Daily update

---
+++ data/summary-1604981401.js
@@ -0,0 +1,45 @@
+$( document ).ready(function() {
+    var data = {
+				    "content": {            
+				               								
+								"count": "323,625", 
+								"death": "7,177" ,
+								"dailycount": "1,431",
+								"dailydeath": "5" ,
+								"date":"11/09" ,
+								"info":"through 8:00pm 11/08/2020" ,
+								
+								"testingaverage":"15,779" , 
+								"peopletested":"3,269,035" , 
+								"positivity":"4.1%" , 
+								"hospitalizations":"855"	
+
+								
+				        }       
+				};	
+				
+				
+				var content = data.content;
+				
+				$('#ctn').html(content.count);
+				$('#det').html(content.death);
+				$('#dctn').html(content.dailycount);
+				$('#ddet').html(content.dailydeath);
+				$('#dte').html(content.date);
+				$('#dte_1').html(content.date);
+				$('#dte_2').html(content.date);
+				$('#dte_3').html(content.date);
+				$('#cse').html(content.info);	
+				
+				$('#tstavg').html(content.testingaverage);	
+				$('#ppltst').html(content.peopletested);	
+				$('#pos').html(content.positivity);	
+				$('#hosp').html(content.hospitalizations);
+			
+							
+
+
+});
+
+
+
