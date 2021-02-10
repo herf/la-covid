654d6e19c499cf79e8df54755045da6c6339d406
Michael Herf <mbherf@gmail.com>
2021-02-09 16:00:19 -0800

    Daily update

---
+++ data/summary-1612915201.js
@@ -0,0 +1,55 @@
+$( document ).ready(function() {
+    var data = {
+				    "content": { 
+								
+								"count": "1,152,239", 
+
+								"death": "18,360", 
+
+								"dailycount": "3,353", 
+
+								"dailydeath": "227", 
+
+								"date":"02/09" ,
+								"info":"through 6:00pm 02/08/2021" ,
+								
+								//update M-F only with same info as above,
+								"datem":"02/09" ,
+								"infom":"through 6:00pm 02/08/2021" ,
+								
+								"testingaverage":"67,599" ,
+								"peopletested":"5,622,335" ,
+								"positivity":"8.80%" ,	
+								"hospitalizations":"4,079" 
+									
+				        }       
+				};	
+				
+				
+				var content = data.content;
+				
+				$('#ctn').html(content.count);
+				$('#det').html(content.death);
+				$('#dctn').html(content.dailycount);
+				$('#ddet').html(content.dailydeath);
+				$('#dte').html(content.date);
+				$('#dte_1').html(content.date);
+				$('#dte_2').html(content.date);
+				$('#dte_3').html(content.date);
+				$('#cse').html(content.info);	
+				
+				$('#dte_m').html(content.datem);
+				$('#cse_m').html(content.infom);
+				
+				$('#tstavg').html(content.testingaverage);	
+				$('#ppltst').html(content.peopletested);	
+				$('#pos').html(content.positivity);	
+				$('#hosp').html(content.hospitalizations);
+			
+							
+
+
+});
+
+
+
