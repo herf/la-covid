004692cecc45208bddfb9789d32a59a01ace4203
Michael Herf <mbherf@gmail.com>
2021-04-25 16:00:23 -0700

    Daily update

---
+++ data/summary-1619391602.js
@@ -0,0 +1,67 @@
+$( document ).ready(function() {
+    var data = {
+				    "content": { 
+								
+								
+								
+								
+								
+								
+"count": "1,231,532",
+
+
+
+								"death": "23,773",
+
+
+								"dailycount": "408",
+
+
+								"dailydeath": "5", 
+
+								"date":"04/25" ,
+								"info":"through 6:00pm 04/24/2021" ,
+								
+								//update M-F only with same info as above,
+								"datem":"04/23" ,
+								"infom":"through 6:00pm 04/22/2021" ,
+
+								"testingaverage":"62,856",
+								"peopletested":"6,397,882",
+
+								"positivity":"0.90%" ,	
+								"hospitalizations":"444"
+								
+								
+								
+								}       
+				};	
+				
+				
+				var content = data.content;
+				
+				$('#ctn').html(content.count);
+				$('#det').html(content.death);
+				$('#dctn').html(content.dailycount);
+				$('#ddet').html(content.dailydeath);
+				$('#dte').html(content.date);
+				$('#dte_1').html(content.date);
+				$('#dte_2').html(content.date);
+				$('#dte_3').html(content.date);
+				$('#cse').html(content.info);	
+				
+				$('#dte_m').html(content.datem);
+				$('#cse_m').html(content.infom);
+				
+				$('#tstavg').html(content.testingaverage);	
+				$('#ppltst').html(content.peopletested);	
+				$('#pos').html(content.positivity);	
+				$('#hosp').html(content.hospitalizations);
+			
+							
+
+
+});
+
+
+
