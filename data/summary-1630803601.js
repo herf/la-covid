77cd590c4ae0626f388e63efbd0ed155aff4fd71
Michael Herf <mbherf@gmail.com>
2021-09-04 18:00:25 -0700

    Daily update

---
+++ data/summary-1630803601.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+    var data = {
+				    "content": { 
+
+			"count": "1,416,919",
+
+			"death": "25,434",
+
+			"dailycount": "2,097",
+
+
+			"dailydeath": "36",
+		
+			"date": "09/04" ,
+			"info": "through 6:00pm 09/03/2021",
+
+
+			"testingaverage": "144,706",
+
+
+			"peopletested": "8,208,004",
+
+			"positivity": "2.09%",
+			"hospitalizations": "1,593" 
+
+
+
+								}       
+				};	
+				
+				
+				var content = data.content;
+				
+				$('#ctn').html(content.count);
+				$('#det').html(content.death);
+				$('#dctn').html(content.dailycount);
+				$('#ddet').html(content.dailydeath);
+				$('#dte').html(content.date);
+				$('#dte_1').html(content.date);
+				$('#dte_2').html(content.date);
+				$('#dte_3').html(content.date);
+				$('#cse').html(content.info);	
+			
+				
+				$('#tstavg').html(content.testingaverage);	
+				$('#ppltst').html(content.peopletested);	
+				$('#pos').html(content.positivity);	
+				$('#hosp').html(content.hospitalizations);
+			
+							
+
+
+});
+
+
+
