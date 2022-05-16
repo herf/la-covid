41fe2dc5654209d721e51b1e8d689543d943c3c5
Michael Herf <mbherf@gmail.com>
2022-05-16 16:01:44 -0700

    Daily update

---
+++ data/summary-1652742001.js
@@ -0,0 +1,57 @@
+$( document ).ready(function() {
+    var data = {
+				    "content": { 
+		
+			"count": "2,915,694",
+
+
+			"death": "32,037",
+
+
+			"dailycount": "1,941",
+
+
+			"dailydeath": "5",
+
+
+			"date": "05/16",
+			"info": "through 6:00pm 05/15/2022",
+
+			"testingaverage": "131,450",
+
+			"peopletested": "11,936,751",
+
+			"positivity": "2.36%",
+
+			"hospitalizations": "312",
+			"datehospitalizations": "05/15"
+
+				}       
+				};	
+				
+				
+				var content = data.content;
+				
+				$('#ctn').html(content.count);
+				$('#det').html(content.death);
+				$('#dctn').html(content.dailycount);
+				$('#ddet').html(content.dailydeath);
+				$('#dte').html(content.date);
+				$('#dte_1').html(content.date);
+				$('#dte_2').html(content.date);
+				$('#dte_3').html(content.datehospitalizations);
+				$('#cse').html(content.info);	
+			
+				
+				$('#tstavg').html(content.testingaverage);	
+				$('#ppltst').html(content.peopletested);	
+				$('#pos').html(content.positivity);	
+				$('#hosp').html(content.hospitalizations);
+			
+							
+
+
+});
+
+
+
